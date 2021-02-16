b4fced870772efc7e98a93454c320a706cf1baf0
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-16 18:14:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  read_from_hash(:name, "Devon")
-  read_from_hash(:age, "100")
+  hash [key]
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  if hash[key
+  else
+  end]
 end
