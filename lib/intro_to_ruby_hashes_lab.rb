604714dc56d3441458dc654abe74d2e55cdf2e3b
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-16 17:28:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -4,7 +4,9 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash_to_read = {location: "NYC", occupation: "Student"}
+  read_from_hash(hash_to_read, :location)
+  read_from_hash(hash_to_read, :occupation)
 end
 
 def update_counting_hash(hash, key)
